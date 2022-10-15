8aa5616d426820eca249b2c7d9558f937a02b6a8
Alexandre Delétraz <alexandre.deletraz@hotmail.com>
2022-10-15 14:40:41 +0200

    Modifications des textes par des const string.

--- main.cpp
+++ main.cpp
@@ -13,10 +13,11 @@ Remarque(s) 	      : Le programme ne gère pas les erreurs de saisie (caractère
 Compilateur 	      : Mingw-w64 g++ 12.1.0
 -----------------------------------------------------------------------------------*/
 
-#include <iomanip>	// setw(...) et setprecision(...)
-#include <cstdlib>	// EXIT_SUCCESS
+#include <iomanip>  // setw(...) et setprecision(...)
+#include <cstdlib>  // EXIT_SUCCESS
 #include <iostream> // cout et cin
-#include <limits>	// numeric_limits<streamsize>
+#include <limits>	  // numeric_limits<streamsize>
+#include <string>   // to work with strings
 
 #define CLEAR_BUFFER cin.ignore(numeric_limits<streamsize>::max(), '\n')
 
@@ -35,35 +36,67 @@ int main()
    const int 	 HOURS_BEGIN_DAY    = 8;
    const int 	 HOURS_FINISH_DAY   = 20;
 
+   //Text messages
+
+   const string WELCOME_MSG      = "\nBonjour, ce programme permet de calculer\n"
+                                     "le prix d'une course de taxi.\n"
+                                     "Lorsqu'il vous sera demander l'heure\n"
+                                     "de depart, veuillez saisir l'heure en format"
+                                     " hh:mm.\n"
+                                     "Voici les conditions      "s;
+   const string TAX_BASE_MSG     = " - prise en charge  : "s;
+   const string TAX_BAG_MSG      = " - supp par bagage  : "s;
+   const string PRICE_DAY_MSG    = " - tarif/min (jour) : "s;
+   const string PRICE_NIGHT_MSG  = " - tarif/min (nuit) : "s;
+   const string DAY_SCHEDULE_MSG = " - heures de jour   : "s;
+   const string BAG_ERROR_MSG    = "Erreur : le nombre de bagage doit etre compris"
+                                   " entre "s;
+   const string DIST_ERROR_MSG   = "Erreur : la distance doit etre comprise entre "s;
+   const string SPEED_ERROR_MSG  = "Erreur : la vitesse doit etre comprise entre "s;
+   const string TIME_ERROR_MSG   = "Erreur : l'heure de depart doit etre comprise "
+                                   "entre "s;
+   const string ORDER_MSG        = "Votre commande"s;
+   const string BAG_ORDER_MSG    = "- nbre de bagage     "s;
+   const string DIST_ORDER_MSG   = "- distance [km]    "s;
+   const string FRENCH_AND       = " et "s;
+   const string SPEED_ORDER_MSG  = "- vitesse         "s;
+   const string DEP_ORDER_MSG    = "- depart            "s;
+   const string TIME_FORMAT_MSG  = "[hh:mm]"s;
+   const string TICKET_MSG       = "Votre ticket"s;
+   const string EXIT_MSG         = "Presser ENTRER pour quitter"s;
+   const string TICKET_BASE_MSG  = " - prise en charge  	: "s;
+   const string TICKET_BAG_MSG   = " - supp bagages    	: "s;
+   const string TICKET_PRICE_MSG = " - prix de la course 	: "s;
+   const string TICKET_TOTAL_MSG = "TOTAL : "s;
+
+   //Symboles
+   const string OPEN_INTER_SYMB        = "["s;
+   const string CLOSE_INTER_SYMB       = "]"s;
+   const string WELCOME_SEPARATION_SYM = "=========================="s;
+   const string MID_VALUE_SEPARATION_SYM = " - "s;
+   const string ORDER_SEPARATION_SYM = "=============="s;
+   const string DOUBLE_DOT           = " : "s;
+   const string TICKET_SEPARATION_SYM = "============"s;
+
+
    // Others
    const int 	 WIDTH              = 8;
    const int	 WIDTHTOTALPRICE    = 26;
 
-   //-------------------------------------------------------------
-   // Initializing variables
-   //-------------------------------------------------------------
-
-   // Others
-   bool   checkMistake = true;
-
-   // Setting the precision of the output and aligns it to the left
+  // Setting the precision of the output and aligns it to the left
    cout << fixed << setprecision(2) << left;
 
    //-------------------------------------------------------------
    //                          Welcome
    //-------------------------------------------------------------
-   cout << "Bonjour, ce programme permet de calculer le prix d'une"      << endl
-        << " course de taxi."                                            << endl
-        << "Lorsque'il vous sera demander l'heure de depart, veuilliez"  << endl
-        << "saisir l'heure en format hh:mm."                             << endl
-        << "Voici les conditions      "                                  << endl
-        << "=========================="                                  << endl
-        << " - prise en charge  : " << setw(WIDTH) << PRICE_TAX_BASE 	    << endl
-        << " - supp par bagage  : " << setw(WIDTH)	<< PRICE_TAX_BAG 		<< endl
-        << " - tarif/min (jour) : " << setw(WIDTH)	<< PRICE_MINUTE_DAY 	<< endl
-        << " - tarif/min (nuit) : " << setw(WIDTH)	<< PRICE_MINUTE_NIGHT   << endl
-        << " - heures de jour   : " << "["         << HOURS_BEGIN_DAY    << " - "
-        << HOURS_FINISH_DAY         << "]"         << endl               << endl;
+   cout << WELCOME_MSG            << endl
+        << WELCOME_SEPARATION_SYM << endl
+        << TAX_BASE_MSG           << setw(WIDTH)      << PRICE_TAX_BASE     << endl
+        << TAX_BAG_MSG            << setw(WIDTH)      << PRICE_TAX_BAG      << endl
+        << PRICE_DAY_MSG          << setw(WIDTH)      << PRICE_MINUTE_DAY   << endl
+        << PRICE_NIGHT_MSG        << setw(WIDTH)      << PRICE_MINUTE_NIGHT << endl
+        << DAY_SCHEDULE_MSG       << OPEN_INTER_SYMB  << HOURS_BEGIN_DAY    << MID_VALUE_SEPARATION_SYM
+        << HOURS_FINISH_DAY       << CLOSE_INTER_SYMB << endl               << endl;
 
    //-------------------------------------------------------------
    // Ask the user to enter their order
@@ -85,17 +118,19 @@ int main()
    double finalTotal;
 
    // Users min/max inputs
-   const int 	 MIN_BAG   = 0;
-   const int 	 MAX_BAG   = 4;
-   const int 	 MIN_KM    = 0;
-   const int 	 MAX_KM	  = 500;
-   const int 	 MIN_SPEED = 50;
-   const int 	 MAX_SPEED = 120;
+   const int MIN_BAG   = 0;
+   const int MAX_BAG   = 4;
+   const int MIN_KM    = 0;
+   const int MAX_KM	  = 500;
+   const int MIN_SPEED = 50;
+   const int MAX_SPEED = 120;
 
 
-   cout << "Votre commande"         << endl
-        << "=============="         << endl
-        << "- nbre de bagage     [" << MIN_BAG << " - " << MAX_BAG << "] : ";
+   cout << ORDER_MSG                   << endl
+        << ORDER_SEPARATION_SYM        << endl
+        << BAG_ORDER_MSG    << OPEN_INTER_SYMB << MIN_BAG
+        << MID_VALUE_SEPARATION_SYM  << MAX_BAG
+        << CLOSE_INTER_SYMB << DOUBLE_DOT;
    cin  >> numberBag;
    CLEAR_BUFFER;
 
@@ -106,48 +141,35 @@ int main()
    }
    else
    {
-       cout << "Erreur : le nombre de bagage doit etre compris entre "
-            << MIN_BAG << " et " << MAX_BAG << endl;
-       checkMistake = false;
+       cout << BAG_ERROR_MSG << MIN_BAG << FRENCH_AND << MAX_BAG << endl;
+       return EXIT_FAILURE;
    }
 
-
-    cout << "- distance [km]    [" << MIN_KM << " - " << MAX_KM << "] : ";
+    cout << DIST_ORDER_MSG << OPEN_INTER_SYMB << MIN_KM
+         << MID_VALUE_SEPARATION_SYM << MAX_KM << CLOSE_INTER_SYMB << DOUBLE_DOT;
     cin  >> distanceTrip;
     CLEAR_BUFFER;
 
     // check if the distance is between MIN_KM and MAX_KM
-    if (distanceTrip >= MIN_KM and distanceTrip <= MAX_KM)
+    if (!(distanceTrip >= MIN_KM and distanceTrip <= MAX_KM))
     {
-
+       cout << DIST_ERROR_MSG << MIN_KM << FRENCH_AND << MAX_KM << endl;
+       return EXIT_FAILURE;
     }
 
-    else
-    {
-        cout << "Erreur : la distance doit etre comprise entre "
-             << MIN_KM << " et " << MAX_KM << endl;
-        checkMistake = false;
-    }
-
-
-    cout << "- vitesse         [" << MIN_SPEED << " - " << MAX_SPEED << "] : ";
+    cout << SPEED_ORDER_MSG << OPEN_INTER_SYMB << MIN_SPEED << MID_VALUE_SEPARATION_SYM
+         << MAX_SPEED << CLOSE_INTER_SYMB << DOUBLE_DOT;
     cin  >> averageSpeed;
     CLEAR_BUFFER;
 
     // check if the speed is between MIN_SPEED and MAX_SPEED
-    if (averageSpeed >= MIN_SPEED and averageSpeed <= MAX_SPEED)
+    if (!(averageSpeed >= MIN_SPEED and averageSpeed <= MAX_SPEED))
     {
-
-    }
-    else
-    {
-        cout << "Erreur : la vitesse doit etre comprise entre "
-             << MIN_SPEED << " et " << MAX_SPEED << endl;
-        checkMistake = false;
+       cout << SPEED_ERROR_MSG << MIN_SPEED << FRENCH_AND << MAX_SPEED << endl;
+       return EXIT_FAILURE;
     }
 
-
-    cout << "- depart            [hh:mm] : ";
+    cout << DEP_ORDER_MSG << TIME_FORMAT_MSG << DOUBLE_DOT;
     cin  >> hourBegin >> minuteBegin;
     depTimeInMinutes = hourBegin * HOUR_TO_MINUTE + minuteBegin;
     // Insert a blank line for a better reading on the output
@@ -175,22 +197,56 @@ int main()
     }
     // else take night prices
     else if (depTimeInMinutes <  HOURS_BEGIN_DAY  * HOUR_TO_MINUTE or
-         depTimeInMinutes >= HOURS_FINISH_DAY * HOUR_TO_MINUTE)
+             depTimeInMinutes >= HOURS_FINISH_DAY * HOUR_TO_MINUTE)
     {
+//       // dépassement si départ avant minuit.
+//       if((depTimeInMinutes + travelTimeInMinutes) - MINUTES_IN_DAY >
+//          HOURS_BEGIN_DAY)
+//       {
+//          tripTotal = (depTimeInMinutes   + travelTimeInMinutes -
+//                       MINUTES_IN_DAY - HOURS_BEGIN_DAY * HOUR_TO_MINUTE) *
+//                      PRICE_MINUTE_DAY +
+//
+//                      (MINUTES_IN_DAY + HOURS_BEGIN_DAY * HOUR_TO_MINUTE -
+//                       depTimeInMinutes) * PRICE_MINUTE_NIGHT;
+//       }
+//          // dépassement si départ après minuit
+//       else if (depTimeInMinutes + travelTimeInMinutes > HOURS_BEGIN_DAY * HOUR_TO_MINUTE)
+//       {
+//          tripTotal = (depTimeInMinutes + travelTimeInMinutes -
+//                       HOURS_BEGIN_DAY * HOUR_TO_MINUTE) * PRICE_MINUTE_DAY +
+//
+//                      (HOURS_BEGIN_DAY - depTimeInMinutes) * PRICE_MINUTE_NIGHT;
+//       }
+//
+//       else
+//       {
+//          // Casting to double to avoid integer division
+//          tripTotal = travelTimeInMinutes * PRICE_MINUTE_NIGHT;
+//       }
+       // If the trip exceeds midnight
         if (depTimeInMinutes + travelTimeInMinutes >= MINUTES_IN_DAY)
         {
-            tripTotal = //de 20 h a minuit.
-                    (MINUTES_IN_DAY - depTimeInMinutes +
-                     travelTimeInMinutes -
-                     (MINUTES_IN_DAY - depTimeInMinutes)) *
-                    PRICE_MINUTE_NIGHT;
-            if(MINUTES_IN_DAY - (depTimeInMinutes+ travelTimeInMinutes) > HOURS_BEGIN_DAY)
+            tripTotal = //from 20 h to midnight.
+                        (MINUTES_IN_DAY - depTimeInMinutes +
+                        //after midnight
+                         travelTimeInMinutes - (MINUTES_IN_DAY - depTimeInMinutes)) *
+                         PRICE_MINUTE_NIGHT;
+            //if the trip start before midnight and finish in the morning
+            if ((depTimeInMinutes + travelTimeInMinutes) - MINUTES_IN_DAY >
+                HOURS_BEGIN_DAY)
             {
+                              //calculate how much time of day we have to add.
                 tripTotal += (depTimeInMinutes  + travelTimeInMinutes -
                               HOURS_BEGIN_DAY   * HOUR_TO_MINUTE)     *
                               PRICE_MINUTE_DAY;
             }
         }
+//        else if (0 <= depTimeInMinutes and depTimeInMinutes < HOURS_BEGIN_DAY *
+//                 HOUR_TO_MINUTE)
+//        {
+//
+//        }
 
         else
         {
@@ -201,47 +257,36 @@ int main()
     else
     {	cout << "Erreur : l'heure de depart doit etre comprise entre "
             "0 et 23" << endl;
-    checkMistake = false;
+       return EXIT_FAILURE;
     }
 
 
    //-------------------------------------------------------------
    // If all values are correct, calculate the total
    //-------------------------------------------------------------
-   if (checkMistake)
-   {
-      finalTotal = PRICE_TAX_BASE + BagTotal + tripTotal;
 
-      cout << "votre Ticket"        << endl
-           << "============"        << endl
-           << " - prise en charge  	: "           << setw(WIDTH) << right
-           << PRICE_TAX_BASE        << endl
-           << " - supp bagages    	: "           << setw(WIDTH) << right
-           << BagTotal              << endl
-           << " - prix de la course 	: "        << setw(WIDTH) << right
-           << tripTotal 	         << endl
-           << setw(WIDTHTOTALPRICE) << "TOTAL : " << setw(WIDTH) << right
-           << finalTotal 			   << endl;
-   }
+   finalTotal = PRICE_TAX_BASE + BagTotal + tripTotal;
+
+   cout << TICKET_MSG        << endl
+        << TICKET_SEPARATION_SYM        << endl
+        << TICKET_BASE_MSG           << setw(WIDTH) << right
+        << PRICE_TAX_BASE        << endl
+        << TICKET_BAG_MSG          << setw(WIDTH) << right
+        << BagTotal              << endl
+        << TICKET_PRICE_MSG        << setw(WIDTH) << right
+        << tripTotal 	         << endl
+        << setw(WIDTHTOTALPRICE) << TICKET_TOTAL_MSG << setw(WIDTH) << right
+        << finalTotal 			   << endl;
 
    //-------------------------------------------------------------
    // End of program
    //-------------------------------------------------------------
-   cout << "Presser ENTRER pour quitter";
+   cout << EXIT_MSG;
    CLEAR_BUFFER;
-
-   /* Chose considérée comme des erreurs :
-    * - utilisé des tabulations au lieu d'espace (corrigé)
-    * - Faire un bitwise au lieu d'un et logique (corrigé)
-    * - imbriqué autant de if pour les tests
-    * */
 
    /*
     * Travail à faire :
-    * Gérer le changement de tarif durant le trajet
-    *
-    * Gérer la saisie de l'heure au format hh.mm. (Normalement bon).
-    *
+    * Gérer le changement de tarif durant le trajet de nuit à jour.
     */
    return EXIT_SUCCESS;
 }
